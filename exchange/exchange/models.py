15f2bafbd987c40bef1bbf5e51f3fe6dcbe6854c
p2pppp <parviz.yusupov3@mail.ru>
2025-07-20 01:09:14 +0500

    Create models.py

---
+++ exchange/exchange/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Currency(models.Model):
+    code = models.CharField(max_length=10, unique=True) # 'USDT', 'BTC', 'ETH'
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.code
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    from_currency = models.ForeignKey(Currency, related_name='from_currency', on_delete=models.CASCADE)
+    to_currency = models.ForeignKey(Currency, related_name='to_currency', on_delete=models.CASCADE)
+    amount = models.FloatField()
+    rate = models.FloatField()
+    result_amount = models.FloatField()
+    commission = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('done', 'Done'), ('failed', 'Failed')], default='pending')
