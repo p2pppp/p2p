411be1c79d0a5b14740b05526ad415a437f7bd2f
p2pppp <parviz.yusupov3@mail.ru>
2025-07-20 22:13:53 +0500

    Update utils.py

--- exchange/utils.py
+++ exchange/utils.py
@@ -4,3 +4,8 @@ def notify_user_email(user, subject, message):
 
 def notify_user_telegram(user, message):
     # используйте user.telegram_id для отправки через Telegram API
+import requests
+def get_binance_price(pair):
+    url = f"https://api.binance.com/api/v3/ticker/price?symbol={pair}"
+    data = requests.get(url).json()
+    return float(data['price'])
